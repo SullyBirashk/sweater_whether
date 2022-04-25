3bb3088754ca988d22bbd96b5b875240fb0624ce
Sully Birashk <sully.birashk@gmail.com>
2022-04-25 08:33:59 -0700

    Facade Test

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe MapFacade do
     expect(coordinates.latitude).to be_a Float
     expect(coordinates.longitude).to be_a Float
   end
+
+  it 'returns travel time' do
+    from = 'denver,co'
+    to = 'pueblo,co'
+
+    travel_time = MapFacade.find_travel_time(from, to)
+    expect(travel_time).to be_a Travel
+    expect(travel_time.time).to be_a String
+    expect(travel_time.time).to eq("01:45:23")
+  end
 end
