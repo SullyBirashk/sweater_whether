f67cb4e75df52b2deb114d2b2babd896129e0065
Sully Birashk <sully.birashk@gmail.com>
2022-04-25 10:30:23 -0700

    Add Id to Serialize

--- app/poros/munchies.rb
+++ app/poros/munchies.rb
@@ -1,7 +1,8 @@
 class Munchies
-  attr_reader :destination_city, :travel_time, :forecast, :restaurant
+  attr_reader :id, :destination_city, :travel_time, :forecast, :restaurant
 
   def initialize(destination_city, travel_time, forecast, restaurant)
+    @id = nil
     @destination_city = destination_city
     @travel_time = travel_time.time
     @forecast = {summary: forecast[:current_weather].conditions,
