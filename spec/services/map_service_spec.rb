c8bef93055afe3cc125f2b7ca7c56adb77774424
Sully Birashk <sully.birashk@gmail.com>
2022-04-25 08:34:05 -0700

    Service Test

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe MapService do
    expect(search[:results][0][:locations][0][:latLng][:lat]).to be_a Float
    expect(search[:results][0][:locations][0][:latLng][:lng]).to be_a Float
  end
+
+ it 'returns travel time given a starting and arriving endpoint' do
+   from = 'denver,co'
+   to = 'pueblo,co'
+
+   search = MapService.get_travel_time(from, to)
+
+   expect(search).to be_a Hash
+   expect(search).to have_key(:route)
+   expect(search[:route]).to be_a Hash
+   expect(search[:route]).to have_key(:formattedTime)
+   expect(search[:route][:formattedTime]).to be_a String
+ end
 end
