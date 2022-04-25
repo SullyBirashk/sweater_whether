5aaa80871027d59323eed337d446c95a92d07f86
Sully Birashk <sully.birashk@gmail.com>
2022-04-24 19:28:48 -0700

    Creeate Image Poro

---
+++ app/poros/image.rb
@@ -0,0 +1,11 @@
+class Image
+  attr_reader :id, :description, :alt_description, :urls
+
+  def initialize(data)
+    @id = data[:id]
+    @description = data[:description]
+    @alt_description = data[:alt_description]
+    @urls = data[:urls]
+  end
+
+end
