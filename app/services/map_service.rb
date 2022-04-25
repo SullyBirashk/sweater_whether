4d2cffddaf02cf59b193c28735d271ed619e3763
Sully Birashk <sully.birashk@gmail.com>
2022-04-25 08:34:13 -0700

    Map Service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -12,4 +12,8 @@ class MapService
   def self.get_coordinates(location)
     get_url("/geocoding/v1/address?key=#{ENV['map_api']}&location=#{location}")
   end
+
+  def self.get_travel_time(from, to)
+    get_url("/directions/v2/route?key=#{ENV['map_api']}&from=$#{from}&to=#{to}")
+  end
 end
