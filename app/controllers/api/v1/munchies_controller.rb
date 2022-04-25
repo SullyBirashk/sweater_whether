794ace37cfa0c1b273c24b79543d62488887e72c
Sully Birashk <sully.birashk@gmail.com>
2022-04-25 10:06:51 -0700

    Update Controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def index
-    require "pry"; binding.pry
-    from = params[:start]
-    location = params[:destination]
+    start = params[:start]
+    destination = params[:destination]
     category = params[:food]
+    travel_time = MapFacade.find_travel_time(start, destination)
+    coordinates = MapFacade.find_coordinates(destination)
+    weather = WeatherFacade.find_weather(coordinates.latitude, coordinates.longitude)
+    restaurant = RestaurantFacade.find_restaurants(category, destination)
+    require "pry"; binding.pry
   end
 
 end
