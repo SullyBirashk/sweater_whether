f2a594d1e19b891f2861270274e8a8e1e4f30c20
Sully Birashk <sully.birashk@gmail.com>
2022-04-26 14:43:05 -0700

    refactor hourly weather

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -2,7 +2,7 @@ class HourlyWeather
   attr_reader :time, :temp, :conditions, :icon
 
   def initialize(data)
-    @time = Time.at(data[:dt]).to_datetime.strftime('%D %r')
+    @time = Time.at(data[:dt]).to_datetime.strftime('%r')
     @temp = data[:temp]
     @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
